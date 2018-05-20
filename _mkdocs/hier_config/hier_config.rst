fd8b820c95ad81f1fdbfb5ab0914600d567d6826
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-20 01:38:10 +0000

    Deploy netdevops/hier_config to github.com/netdevops/hier_config.git:gh-pages

---
+++ _mkdocs/hier_config/hier_config.rst
@@ -0,0 +1,37 @@
+hier\_config package
+====================
+
+Subpackages
+-----------
+
+.. toctree::
+
+    hier_config.helpers
+
+Submodules
+----------
+
+hier\_config.hc\_child module
+-----------------------------
+
+.. automodule:: hier_config.hc_child
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+hier\_config.text\_match module
+-------------------------------
+
+.. automodule:: hier_config.text_match
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+Module contents
+---------------
+
+.. automodule:: hier_config
+    :members:
+    :undoc-members:
+    :show-inheritance:
---
+++ _mkdocs/mkdocs.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# create python virtual env
+python3 -m venv sphinx-env
+source ./sphinx-env/bin/activate
+../../setup.py install
+pip3 install sphinx sphinx_rtd_theme
+
+# Automatically create code documentation
+sphinx-apidoc -f -o hier_config ../../hier_config
+
+# Remove exisitng code table of contents
+if [ -f hier_config.rst ]; then
+    rm hier_config.rst
+fi
+
+# Create a new code table of contents
+cat > hier_config.rst << EOF
+.. _hier_config:
+
+=============================================
+Hierarchical Configuration Code Documentation
+=============================================
+
+.. toctree::
+   :maxdepth: 1
+
+EOF
+
+for i in `ls hier_config`; do
+  echo "   hier_config/$i" >> hier_config.rst
+done
+
+# Create the html files
+make clean
+make dirhtml
+
+# Sync generated documentation to viewable path
+rsync -avz --exclude sphinx-env "${PWD}/_build/dirhtml/" "../../docs"
+
+# remove python env
+deactivate
+rm -rf ./sphinx-env
